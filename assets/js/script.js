be5c5384b54706138a87c9ea4ad53333e9cff26b
Melhefnawi <>
2023-04-26 20:53:30 +0000

    adding the return color function

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,24 +31,22 @@ document.addEventListener("DOMContentLoaded", function () {
  */
 function runGame(gameType) {
 
-    document.getElementById("answer-box").value = "";
-    document.getElementById("answer-box").focus();
+    // document.getElementById("answer-box").value = "";
+    // document.getElementById("answer-box").focus();
 
     // Creates two random numbers between 1 and 25
-    let num1 = Math.floor(Math.random() * 23) + 1;
-    let num2 = Math.floor(Math.random() * 25) + 1;
+    let num1 = Math.floor(Math.random() * 20) + 1;
+
     let colors = ["aqua", "beige", "black", "gold", "indigo", "magenta", "maroon", "navy", "olive", "orange", "peachpuff", "pink", "purple", "red", "gray", "violet", "yellow", "salmon", "turquoise"];
     let color = colors[num1];
-    if (gameType === "red") {
-        displayRedColor(num1, num2);
-    } else if (gameType === "multiply") {
-        displayMultiplyQuestion(num1, num2);
-    } else if (gameType === "subtract") {
-        displaySubtractQuestion(num1, num2);
-    } else {
-        alert(`Unknown game type: ${gameType}`);
-        throw `Unknown game type: ${gameType}. Aborting!`;
-    }
+    setColor(color);
+    returnColor();
+    //if (gameType === "red") {
+    // displayRedColor();
+    //} else {
+    //   alert(`Unknown game type: ${gameType}`);
+    //  throw `Unknown game type: ${gameType}. Aborting!`;
+    //}
 
 }
 
@@ -56,88 +54,95 @@ function runGame(gameType) {
  * Checks the answer against the first element in
  * the returned calculateCorrectAnswer array
  */
-function checkAnswer() {
+//function checkAnswer() {
 
-    let userAnswer = parseInt(document.getElementById("answer-box").value);
-    let calculatedAnswer = calculateCorrectAnswer();
-    let isCorrect = userAnswer === calculatedAnswer[0];
+// let userAnswer = parseInt(document.getElementById("answer-box").value);
+// let calculatedAnswer = calculateCorrectAnswer();
+// let isCorrect = userAnswer === calculatedAnswer[0];
 
-    if (isCorrect) {
-        alert("Hey! You got it right! :D");
-        incrementScore();
-    } else {
-        alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
-        incrementWrongAnswer();
-    }
+// if (isCorrect) {
+//     alert("Hey! You got it right! :D");
+//     incrementScore();
+// } else {
+//     alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+//      incrementWrongAnswer();
+// }
 
-    runGame(calculatedAnswer[1]);
+//runGame(calculatedAnswer[1]);
 
-}
+//}
 
 /**
  * Gets the operands (the numbers) and the operator (plus, minus etc)
  * directly from the dom, and returns the correct answer.
  */
-function calculateCorrectAnswer() {
+//function calculateCorrectAnswer() {
 
-    let operand1 = parseInt(document.getElementById('operand1').innerText);
-    let operand2 = parseInt(document.getElementById('operand2').innerText);
-    let operator = document.getElementById("operator").innerText;
+// let operand1 = parseInt(document.getElementById('operand1').innerText);
+// let operand2 = parseInt(document.getElementById('operand2').innerText);
+// let operator = document.getElementById("operator").innerText;
 
-    if (operator === "+") {
-        return [operand1 + operand2, "addition"];
-    } else if (operator === "x") {
-        return [operand1 * operand2, "multiply"];
-    } else if (operator === "-") {
-        return [operand1 - operand2, "subtract"];
-    } else {
-        alert(`Unimplemented operator ${operator}`);
-        throw `Unimplemented operator ${operator}. Aborting!`;
-    }
+// if (operator === "+") {
+//     return [operand1 + operand2, "addition"];
+// } else if (operator === "x") {
+//     return [operand1 * operand2, "multiply"];
+// } else if (operator === "-") {
+//     return [operand1 - operand2, "subtract"];
+// } else {
+//    alert(`Unimplemented operator ${operator}`);
+//    throw `Unimplemented operator ${operator}. Aborting!`;
+// }
 
-}
+//}
 
 /**
  * Gets the current score from the DOM and increments it by 1
  */
-function incrementScore() {
+//function incrementScore() {
 
-    let oldScore = parseInt(document.getElementById("score").innerText);
-    document.getElementById("score").innerText = ++oldScore;
+//let oldScore = parseInt(document.getElementById("score").innerText);
+//document.getElementById("score").innerText = ++oldScore;
 
-}
+//}
 
 /**
  * Gets the current tally of incorrect answers from the DOM and increments it by 1
  */
-function incrementWrongAnswer() {
+//function incrementWrongAnswer() {
 
-    let oldScore = parseInt(document.getElementById("incorrect").innerText);
-    document.getElementById("incorrect").innerText = ++oldScore;
+//let oldScore = parseInt(document.getElementById("incorrect").innerText);
+// document.getElementById("incorrect").innerText = ++oldScore;
 
-}
+//}
 
-function displayRedColor() {
+function setColor(color) {
 
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = "+";
+    let input = document.getElementById('answer-box');
+    input.style.backgroundColor = color;
 
-
-}
-
-function displaySubtractQuestion(operand1, operand2) {
-
-    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
-    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
-    document.getElementById('operator').textContent = "-";
 
 }
 
-function displayMultiplyQuestion(operand1, operand2) {
-
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = "x";
-
+function returnColor() {
+    let textColor = document.getElementById('txt').value;
+    if (textColor === "") {
+        alert(`Please enter the correct color & press submit`);
+    } else {
+        return textColor;
+    }
 }
+//function displaySubtractQuestion(operand1, operand2) {
+
+    //document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    //document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+   // document.getElementById('operator').textContent = "-";
+
+//}
+
+//function displayMultiplyQuestion(operand1, operand2) {
+
+    //document.getElementById('operand1').textContent = operand1;
+    //document.getElementById('operand2').textContent = operand2;
+    //document.getElementById('operator').textContent = "x";
+
+//}
